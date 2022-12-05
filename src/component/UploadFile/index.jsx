651aab8355565a95747d30229fc840db5475f825
Ke <zhangke806918@gmail.com>
2022-12-05 15:44:48 +0100

    Upload first commit

--- src/component/UploadFile/index.jsx
+++ src/component/UploadFile/index.jsx
@@ -1,24 +1,27 @@
 import React, {Component} from 'react';
-import {Button, Form, Input} from "antd";
-
+import {Button, Form, Input,} from "antd";
 import This from "./index.module.css"
 
-class Index extends Component {
+
+
+class Upload extends Component {
     constructor(props) {
         super(props);
         this.$refs = {}
+
+        this.onFinish = this.onFinish.bind(this)
     }
 
     render() {
         return (
             <div>
                 <Form onFinish={this.onFinish} className={This['demo-form-inline']}>
-                    <Form.Item label="文件" name="file">
-                        <input type="file" ref={c => this.$refs.file = c} className={This.input}/>
+                    <Form.Item label="Daten hochladen" name="file">
+                        <Input type="file" ref={c => this.$refs.file = c} className={This.input}/>
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit" className={This.submit}>
-                            上传
+                        Bestätigt
                         </Button>
                     </Form.Item>
                 </Form>
@@ -27,9 +30,18 @@ class Index extends Component {
             ;
     }
 
+    // 回调函数，发送请求逻辑需要写在这里
     onFinish() {
-        console.log("finish")
+        // 获取用户输入
+        const {file} = this.$refs
+        try {
+            console.log(file.files)
+        }catch (e) {
+
+        }
+
     }
 }
 
-export default Index;
+
+export default Upload;
